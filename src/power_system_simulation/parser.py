957a0cf9f2e0027ba31c507defc6ae92934ac33c
poorterj <jarno.depoorter.jdp@gmail.com>
2026-05-30 01:18:07 +0200

    Small additions

--- src/power_system_simulation/parser.py
+++ src/power_system_simulation/parser.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 from pathlib import Path
-from json import load
+
 
 # Error classes
 class FileNotFoundError(Exception):
@@ -9,11 +9,17 @@ class FileNotFoundError(Exception):
         self.path = path
     pass
 
+class FileEmptyError(Exception):
+    pass
+
+class DataMissingError(Exception):
+    pass
+
 # Object structures
 
 class PowerNode:
-    def __init__(self, u_rated: int):
-        self.u_rated: int = u_rated
+    def __init__(self, u_rated: float) -> None:
+        self.u_rated: float = u_rated
         pass
 
 class PowerLine:
@@ -63,9 +69,10 @@ class PowerGridModelData:
         self.nodes: defaultdict[int, PowerNode] = defaultdict(list)
         self.lines: defaultdict[int, PowerLine] = defaultdict(list)
         self.load: defaultdict[int, PowerLoad] = defaultdict(list)
+        self.source: PowerSource = None
+
         self.p_profile: defaultdict[str, PowerProfilePerTimestamp] = defaultdict(list)
         self.q_profile: defaultdict[str, PowerProfilePerTimestamp] = defaultdict(list)
-        self.source: PowerSource = None
 
         self.load_model()
 
@@ -76,8 +83,17 @@ class PowerGridModelData:
         PowerGridModelData._does_file_exist(self.p_profile_path)
 
         # Parse file network
-        with open(self.network_path, 'r', encoding='utf-8') as f:
-            network_data = json.load(f)
+        with open(self.network_path, encoding='utf-8') as file:
+            network_data = json.load(file).get("data", {})
+
+        # Check if file contains data
+        PowerGridModelData._CheckFileContainsData(network_data)
+
+        # Parse data into structured objects
+        self.nodes: defaultdict[int, PowerNode] = PowerGridModelData._ParseNodes(network_data) # Parse nodes
+        self.lines: defaultdict[int, PowerLine] = PowerGridModelData._ParseLines(network_data) # Parse lines
+        self.load: defaultdict[int, PowerLoad] = PowerGridModelData._ParseLoads(network_data) # Parse loads
+        self.source: PowerSource = PowerGridModelData._ParseSource(network_data) # Parse source
         # Set is_populated to True if parsing is successful, otherwise False
 
     @staticmethod
@@ -88,5 +104,32 @@ class PowerGridModelData:
             raise FileNotFoundError(path=path_str, message=f"File not found at path: {path_str}")
 
         return True
-    
-    def  
+
+    @staticmethod
+    def _IsDictEmpty(data: dict) -> bool:
+        return not bool(data)
+
+    @staticmethod
+    def  _CheckFileContainsData(data: dict) -> None:
+        if PowerGridModelData._IsDictEmpty(data):
+            raise FileEmptyError("File does not contain data.")
+        return True
+
+    @staticmethod
+    def _CheckSectionContainsData(section_data: dict, section: str) -> None:
+        if PowerGridModelData._IsDictEmpty(section_data):
+            raise DataMissingError(f"Section \"{section}\" does not contain data.")
+        return True
+
+    @staticmethod
+    def _ParseNodes(network_data: dict) -> defaultdict[int, PowerNode]:
+        # Load and check section data
+        entries = network_data.get("nodes",[])
+        PowerGridModelData._CheckSectionContainsData(entries, "nodes")
+
+        # Prepare structured data
+        nodes: defaultdict[int, PowerNode] = defaultdict(list)
+
+        # Parse entries into structured objects
+        for entry in entries:
+            
